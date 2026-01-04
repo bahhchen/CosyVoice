6ff088223d6434ecd602afc86eedc3048914a964
recode <chenhh@lxspatial.com>
2026-01-04 19:49:54 +0800

    voice
    Signed-off-by: recode <chenhh@lxspatial.com>

--- chh_main.py
+++ chh_main.py
@@ -11,17 +11,34 @@ g_prompts = {
         'prompt_wav' : './chh/assets/zh-ZL2_woman.wav',
         'prompt_text' : '也许你相亲过很多次，可是你有真心付出过吗？你有努力过吗？你一定以为物质就可以换来爱情是吧！你不如去买彩票好了！也许隔天中了奖就可以得到你想要的一切。'
     },
-    'Bowen_man' :{
-        'prompt_wav' : './chh/assets/zh-Bowen_man.wav',
-        'prompt_text' : '简单来说，人工智能是一门致力于让计算机和机器像人一样思考和行动的科学领域。'\
-                        '它的目标，是模拟、延伸和扩展人的智能，让机器能够胜任通常需要人类智慧才能完成的任务。'\
-                        '比如：解决问题、感知环境、理解语言，甚至进行创造。'\
-                        'AI是一非常广泛的领域，它包含了计算机科学、语言学、神经科学，甚至哲学和心理学等多个学科的知识。'
-    },
     'YM_woman' :{
         'prompt_wav' : './chh/assets/zh-YM_woman.wav',
         'prompt_text' : '我醒来时，看见了那个男孩子，他看着我，我想起了和他一起，在养老院晒洗床单的时光。'
-    }
+    },
+    'Xinran_woman':{
+        'prompt_wav' : './chh/assets/zh-Xinran_woman.wav',
+        'prompt_text' : '我喜欢读村上春树，他说如果你爱上了某个星球的一朵花，那么只要在夜晚仰望星空，就会觉得满天的繁星就像一朵朵盛开的花。'
+    },
+
+    'Bowen_man' :{
+        'prompt_wav' : './chh/assets/zh-Bowen_man.wav',
+        'prompt_text' : '简单来说，人工智能是一门致力于让计算机和机器像人一样思考和行动的科学领域。'
+                        # '它的目标，是模拟、延伸和扩展人的智能，让机器能够胜任通常需要人类智慧才能完成的任务。'\
+                        # '比如：解决问题、感知环境、理解语言，甚至进行创造。'\
+                        # 'AI是一非常广泛的领域，它包含了计算机科学、语言学、神经科学，甚至哲学和心理学等多个学科的知识。'
+    },
+    'wyb_man' :{
+        'prompt_wav' : './chh/assets/zh-wyb_man.wav',
+        'prompt_text' : '考拉的一生呢，99%的时间都在吃毒树叶，吃了就睡觉，基本上一辈子都迷迷糊糊的，还有更尴尬的。'
+    },   
+    'XM_man' :{
+        'prompt_wav' : './chh/assets/zh-XM_man.wav',
+        'prompt_text' : '之后会去清点粮食，然后护送队伍，将饱满的麦子、翠绿的蔬菜、肥壮的牛羊等，送到远方的金项岭。'
+    },
+    'zlc_man':{
+        'prompt_wav' : './chh/assets/zh-zlc_man.wav',
+        'prompt_text' : '那接下来呢，便是文武大臣们的发言时间了。哎，他们纷纷拿出那些个尚未解决的、杂七杂八的事件，让皇帝来拿个主意。'
+    },
 }
 
 def save_waves(filename, waves, sample_rate):
@@ -46,7 +63,6 @@ def inference_zero_shot(cosyvoice, txt_contents, prompts, dstwav):
 
     prompts_key0 = next(iter(prompts))
     def_speaker = prompts[prompts_key0]
-    prev_speaker = def_speaker
 
     # "Speaker namexxx: 别整那些没用的了!"
     pattern = r"^Speaker\s+(\w+):\s*(.+)$"    
@@ -59,14 +75,13 @@ def inference_zero_shot(cosyvoice, txt_contents, prompts, dstwav):
             text = m.group(2)           # 别整那些没用的了!
             if speaker_name in prompts:
                 cur_speaker = prompts[speaker_name]
-                prev_speaker = cur_speaker
             else:
-                cur_speaker = prev_speaker
+                cur_speaker = def_speaker
         else:
             text = txt
-            cur_speaker = prev_speaker
+            cur_speaker = def_speaker
 
-        print("speaker_name:", speaker_name)
+        # print("speaker_name:", speaker_name)
         # print("text:", text)
 
         # instruct_list + cur_speaker['prompt_text']
@@ -82,6 +97,34 @@ def inference_zero_shot(cosyvoice, txt_contents, prompts, dstwav):
             waves.append(wav)
 
     save_waves(dstwav, waves, cosyvoice.sample_rate)   
+
+def read_txt_speaker_paragraphs(file_path):
+    """
+    读取 TXT 文件，将段落按 speaker 
+    """
+    paragraphs = []
+    speaker_pattern = re.compile(r'^Speaker\s*\S*:')  # 匹配 Speaker namexxx:
+
+    with open(file_path, "r", encoding="utf-8-sig") as f:
+        noSpeaker = []
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue  # 忽略空行
+
+            if speaker_pattern.match(line):
+                if len(noSpeaker) > 0:
+                    paragraphs.append("。".join(noSpeaker))
+                # 新段落
+                paragraphs.append(line)
+                noSpeaker = []
+            else:
+                noSpeaker.append(line)
+
+        if len(noSpeaker) > 0:
+            paragraphs.append("。".join(noSpeaker))
+
+    return paragraphs
 
 
 def cosyvoice3_example():
@@ -136,6 +179,7 @@ def cosyvoice3_example():
         'Speaker YM_woman: 卧槽! 这谁啊?',
         'Speaker ZL2_woman: 别整那些没用的了!',
     ]
+    txt_contents = read_txt_speaker_paragraphs("./chh/books/1996 终极实验 - 罗伯特·索耶 - fix/0004.1996 终极实验 - 罗伯特·索耶.引子.txt")
     inference_zero_shot(cosyvoice, txt_contents, g_prompts, './chh/output/2p_yayi.wav')
 
 def main():
